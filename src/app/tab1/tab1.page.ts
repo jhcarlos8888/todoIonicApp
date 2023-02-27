4f08dd128b3d48f6713dda4d9311f382f925269f
Juan Carlos Herrera Blandon <jhcarlos8888@gmail.com>
2023-02-27 12:50:32 -0500

    * Se realiza modificacion de labels e iconos en tabs. * Se realiza creacion de evento toast para boton de creacion de una nueva lista

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,57 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
+
+  async AgregarLista(){
+    let alerta = await this.alertController.create({
+      header: "Agregar lista",
+      inputs:[
+        {
+          type: "text",
+          name: "titulo",
+          placeholder: "Ingrese el nombre de la lista"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancelar",
+          role: "cancel"
+        },
+        {
+          text: "Crear",
+          handler: (data:any) => {
+            this.validInput(data);
+            console.log(data);
+          }
+        }
+      ]
+    })
+    
+    await alerta.present()
+  }
+
+  validInput(input: any):boolean{
+
+    if(input && input.titulo){
+      return true;
+      
+    }else{
+      this.presentToast("Por favor ingrese algun valor")
+      return false;
+    }
+  }
+
+  async presentToast(mensaje:string){
+    let toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    })
+    
+    toast.present()
+  }
 
 }
